ae70f19c527d05e42072d043097f789f3ea0a9e2
unknown <rjibon49@gmail.com>
2022-06-13 19:44:03 +0600

    update about page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4d3857a07d2d56a5"; }
+/******/ 		__webpack_require__.h = function() { return "0adf36b35701fecf"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Home/AboutBanner.js
+++ components/Home/AboutBanner.js
@@ -2,15 +2,37 @@ import Image from 'next/image'
 import center from '../../public/src/images/aboutCenter.png'
 import left from '../../public/src/images/aboutLeft.png'
 import right from '../../public/src/images/aboutRight.png'
+import aboutImage from '../../public/src/images/aboutImage.png'
 
 
 const AboutBanner = () => {
     return (
-        <div className="my-3 py-3">
-            <div>
-                <Image src={left} alt="topBannerImage" className='leftImage' />
-                <Image src={center} alt="topBannerImage" className='centerImage' />
-                <Image src={right} alt="topBannerImage" className='rightImage' />
+        <div className="container-fluid my-5 py-5">
+            <div className="container my-5 py-5">
+                <div className="" style={{position:"relative"}}>
+                    <div className='text-center'>
+                        {/* <Image src={left} alt="topBannerImage" className='leftImage' />
+                        <Image src={center} alt="topBannerImage" className='centerImage' />
+                        <Image src={right} alt="topBannerImage" className='rightImage' /> */}
+                        <Image src={aboutImage} alt="topBannerImage" className='aboutImage' />
+                    </div>
+                    <div class="aboutPosiTion">
+                        <div className='row'>
+                            <div className='col-4'>
+                                <p className='about'>About Us</p>
+                                <p className='title'>Creative Designer Creative Website</p>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sem id dolor vitae aliquet placerat ac. </p>
+                            </div>
+
+                            <div className='col-4'></div>
+
+                            <div className='col-4'>
+                                <p className='title'>Let’s Make Something Trendy and Cool</p>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sem id dolor vitae aliquet placerat ac. </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
